6c04b78fcd8df3c4864e17c93c7d658c22feba23
FriedPrawn <wh708@nyu.edu>
2016-05-09 12:49:16 -0400

    Textures

---
+++ NYU Maps/Assets/_Scripts/InGameDBController.cs
@@ -0,0 +1,276 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+using System.Data;
+using Npgsql;
+
+public class InGameDBController : MonoBehaviour 
+{
+	int gameID;
+	int numberOfPlayers; 
+	int pid;
+	public List<string> playerNames;
+	public List<Vector2> playerLocations;
+	int currentPlayerTurn;
+	int winnerPID;
+	String winnerName;
+	
+	Vector2 gridSize;
+	List<string> buildingInitializationData;
+	List<string> taskInitializationData;
+
+	NpgsqlConnection dbcon;
+	NpgsqlCommand dbcmd;
+	NpgsqlDataReader reader;
+	
+	void Start()
+	{
+		playerNames = new List<string>();
+		playerLocations = new List<Vector2>();
+		buildingInitializationData = new List<string>();
+		taskInitializationData = new List<string>();
+		winnerPID = -1;
+	}
+
+	public void StartConnection () 
+	{
+		string connectionString = "Server= pdc-amd01.poly.edu;" +
+								  "Database=kl1983;" +
+								  "User ID=kl1983;" +
+								  "Password=z86udpx%;";
+		
+		dbcon = new NpgsqlConnection(connectionString);
+		dbcon.Open();
+		Debug.Log ("Connected");
+	}
+
+	public void InsertRoomData(int turnID, List<string> insertNames, List<int> insertIDs)
+	{
+		numberOfPlayers = insertNames.Count; 
+		playerNames = insertNames;
+		pid = insertIDs[turnID];
+
+		for(int i = 0; i < numberOfPlayers; i++)
+		{
+			playerLocations.Add(new Vector2(0,0));
+		}
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("INSERT INTO ingameplayer (turnid, pid, ign, roomid, xcoord, ycoord) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}');", 
+		                                    turnID, pid, playerNames[turnID], gameID, playerLocations[turnID].x, playerLocations[turnID].y);
+		reader = dbcmd.ExecuteReader();
+		CleanUpSQLVariables ();
+
+	}
+
+	public void FetchInitialGridData()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("Select * From gridData");
+		reader = dbcmd.ExecuteReader();
+		reader.Read ();
+		gridSize = new Vector2 (reader.GetInt32(0), reader.GetInt32(1));
+		CleanUpSQLVariables ();
+	}
+
+	public void FetchBuildingData()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT name,entrance,lllocation,color,length,width,height FROM ingamebuilding;");
+		reader = dbcmd.ExecuteReader();
+		while(reader.Read()) 
+		{
+			buildingInitializationData.Add(reader.GetString(0) + ";" + reader.GetString(1) + ";" + reader.GetString(2) + ";" + 
+			                               reader.GetString(3) + ";" + reader.GetString(4) + ";" + reader.GetString(5) + ";" + reader.GetString(6));
+		}
+		CleanUpSQLVariables ();
+	}
+
+	public void FetchPlayerLocations()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT xcoord, ycoord FROM ingameplayer WHERE roomid = '{0}' ORDER BY turnid ASC;", gameID);
+		reader = dbcmd.ExecuteReader();
+		int counter = 0;
+		while(reader.Read()) 
+		{
+			playerLocations[counter] = (new Vector2 ((reader.GetInt32(0)), reader.GetInt32(1))); 
+			counter++;
+		}
+		CleanUpSQLVariables ();
+	}
+
+	public void FetchCurrentPlayerTurn()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT currentturn FROM ingameroom WHERE roomid = '{0}';", (int)gameID);
+		reader = dbcmd.ExecuteReader();
+		while(reader.Read()) 
+			currentPlayerTurn = reader.GetInt32(0);
+		CleanUpSQLVariables ();
+	}
+
+	public IEnumerator FetchPlayerLocationsCo()
+	{
+		FetchPlayerLocations ();
+		yield return null;
+	}
+
+	public IEnumerator FetchCurrentPlayerTurnCo()
+	{
+		FetchCurrentPlayerTurn ();
+		yield return null;
+	}
+
+	public IEnumerator FetchWinnderPID()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT winner FROM ingameroom WHERE roomid = '{0}';", gameID);
+		reader = dbcmd.ExecuteReader();
+		reader.Read (); 
+		winnerPID = reader.GetInt32(0);
+		CleanUpSQLVariables ();
+		yield return null;
+	}
+
+	public void FetchTaskData()
+	{
+		taskInitializationData.Clear ();
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT taskname, quantity, buildingname from ingametask");
+		reader = dbcmd.ExecuteReader();
+		while(reader.Read()) 
+			taskInitializationData.Add(reader.GetString(0) + ";" + reader.GetInt32(1) + ";" + reader.GetString(2));
+		CleanUpSQLVariables ();
+	}
+
+	public void IncrementPTurn()
+	{
+		currentPlayerTurn++;
+		if (currentPlayerTurn == numberOfPlayers)
+			currentPlayerTurn = 0;
+
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("UPDATE ingameroom SET currentturn = '{0}' WHERE roomid = '{1}';", 
+		                                    (int)currentPlayerTurn, (int)gameID);
+		reader = dbcmd.ExecuteReader();
+		CleanUpSQLVariables ();
+	}
+
+	public void DecrementBuildingTaskQuantity(string buildingTaskName)
+	{
+		dbcmd = dbcon.CreateCommand(); 
+		dbcmd.CommandText = string.Format ("UPDATE ingametask SET quantity = quantity - 1 WHERE taskname = '{0}'", buildingTaskName);
+		reader = dbcmd.ExecuteReader();
+		CleanUpSQLVariables ();
+	}
+
+	public void MovePlayer(int playerCountID, Vector2 destination)
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("UPDATE ingameplayer SET xcoord = '{0}', ycoord = '{1}' WHERE turnid = '{2}';", 
+		                                    (int)destination.x, (int)destination.y, playerCountID);
+		reader = dbcmd.ExecuteReader();
+		int counter = 0;
+		while(reader.Read()) 
+		{
+			playerLocations[counter] = (new Vector2 ((reader.GetInt32(0)), reader.GetInt32(1))); 
+			counter++;
+		}
+		CleanUpSQLVariables ();
+	}
+
+	public void SetWinner(int localPlayerID)
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("UPDATE ingameroom SET winner = '{0}' WHERE roomid = '{1}';", localPlayerID, gameID);
+		reader = dbcmd.ExecuteReader();
+		CleanUpSQLVariables ();
+	}
+
+	public int GetWinnerPID()
+	{
+		return winnerPID;
+	}
+
+	public string GetWinnerName()
+	{
+		return winnerName;
+	}
+
+	public void FetchWinnerPlayerName()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("SELECT ign FROM users WHERE pid = '{0}';", winnerPID);
+		reader = dbcmd.ExecuteReader();
+		reader.Read ();
+		winnerName = reader.GetString (0);
+		CleanUpSQLVariables ();
+	}
+
+	public int GetCurrentPTurn()
+	{
+		return currentPlayerTurn;
+	}
+
+	public string GetPlayerName(int playerCountID)
+	{
+		return playerNames [playerCountID];
+	}
+
+	public Vector2 GetPlayerLocation(int playerCountID)
+	{
+		return playerLocations [playerCountID];
+	}
+	
+	public Vector2 GetGridSize()
+	{
+		return gridSize;
+	}
+
+	public int GetNumberOfPlayers()
+	{
+		return numberOfPlayers;
+	}
+
+	public List<string> GetBuildingData()
+	{
+		return buildingInitializationData;
+	}
+
+	public List<string> GetTaskData()
+	{
+		return taskInitializationData;
+	}
+
+	public void SetGameID(int id)
+	{
+		gameID = id;
+	}
+
+
+	void CleanUpSQLVariables()
+	{
+		reader.Close();
+		reader = null;
+		dbcmd.Dispose();
+		dbcmd = null;
+	}
+		
+	public void DeleteInGamePlayerData()
+	{
+		dbcmd = dbcon.CreateCommand();
+		dbcmd.CommandText =  string.Format ("DELETE FROM ingameplayer WHERE pid = '{0}';", pid);
+		reader = dbcmd.ExecuteReader();
+		CleanUpSQLVariables ();
+	}
+
+	void OnApplicationQuit()
+	{
+		DeleteInGamePlayerData ();
+		dbcon.Close();
+		dbcon = null;
+		Debug.Log ("Connection Closed");
+	}
+}
---
+++ NYU Maps/Assets/_Scripts/OrientationController.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrientationController : MonoBehaviour 
+{
+	public UIPanel lpTasksPanel;
+	public UIPanel buildingTasksPanel;
+	public UIPanel winnerDisplayPanel;
+
+	public UITexture mainUIPanelBackdrop;
+
+	public GameObject mainUIScalerContainer;
+	public GameObject ipTasksScalerContainer;
+	public GameObject buildingTasksScalerContainer;
+	public GameObject winnerDisplayScalerContainer;
+
+	public UILabel movesLabel;
+	public UIButton rollDiceButton;
+	public UIButton enterBuildingButton;
+	public UIButton endTurnButton;
+
+	public UILabel lpTaskButtonLabel;
+	public UILabel enterBuildingButtonLabel;
+	public UILabel winnerDisplayLabel;
+
+	bool canMovePlayer;
+
+	public void ScaleUI () 
+	{
+		float mainUIScaler = (float)Screen.height / mainUIPanelBackdrop.height;
+		float xShiftToCenterValue = mainUIPanelBackdrop.width * mainUIScaler /2;
+		mainUIScalerContainer.transform.localScale = new Vector3 (mainUIScaler, mainUIScaler, 1);
+		mainUIScalerContainer.transform.localPosition = new Vector3 (-mainUIPanelBackdrop.width*mainUIScaler/2,0,0);
+		ipTasksScalerContainer.transform.localScale = new Vector3 (mainUIScaler, mainUIScaler, 1);
+		ipTasksScalerContainer.transform.localPosition = new Vector3 (-xShiftToCenterValue, 0, 0);
+		buildingTasksScalerContainer.transform.localScale = new Vector3 (mainUIScaler, mainUIScaler, 1);
+		buildingTasksScalerContainer.transform.localPosition = new Vector3 (-xShiftToCenterValue, 0, 0);
+		winnerDisplayScalerContainer.transform.localScale = new Vector3 (mainUIScaler, mainUIScaler, 1);
+		winnerDisplayScalerContainer.transform.localPosition = new Vector3 (-xShiftToCenterValue, 0, 0);
+	}
+
+	public void SetMovesLabel(string content)
+	{
+		movesLabel.text = content;
+	}
+
+	public void SetEndTurnButtonStatus(bool status)
+	{
+		CustomSetUIButtonState (endTurnButton, status);
+	}
+
+	public void SetRollDiceButtonStatus(bool status)
+	{
+		CustomSetUIButtonState (rollDiceButton, status);
+	}
+
+	public void SetEnterBuildingButtonStatus(bool status)
+	{
+		CustomSetUIButtonState (enterBuildingButton, status);
+	}
+
+	public void ToggleLPTasksPanel()
+	{
+		if(lpTasksPanel.alpha == 0)
+		{
+			canMovePlayer = false;
+			lpTaskButtonLabel.text = "Hide Tasks";
+			lpTasksPanel.GetComponent<TweenAlpha>().PlayForward();
+		}
+		else
+		{
+			StartCoroutine(DelayedSetCanMovePlayer(true));
+			lpTaskButtonLabel.text = "Show Tasks";
+			lpTasksPanel.GetComponent<TweenAlpha>().PlayReverse();
+		}
+	}
+
+	public void ToggleBuildingTasksPanel()
+	{
+		if(buildingTasksPanel.alpha == 0)
+		{
+			canMovePlayer = false;
+			enterBuildingButtonLabel.text = "Exit Building";
+			buildingTasksPanel.GetComponent<TweenAlpha>().PlayForward();
+		}
+		else
+		{
+			StartCoroutine(DelayedSetCanMovePlayer(true));
+			enterBuildingButtonLabel.text = "Enter Building";
+			buildingTasksPanel.GetComponent<TweenAlpha>().PlayReverse();
+		}
+	}
+
+	public void ShowWinnerDisplayPanel()
+	{
+		winnerDisplayPanel.GetComponent<TweenAlpha> ().PlayForward ();
+	}
+
+	public bool GetCanMovePlayer()
+	{
+		return canMovePlayer;
+	}
+
+	public void SetCanMovePlayer(bool value)
+	{
+		canMovePlayer = value;
+	}
+
+	public IEnumerator DelayedSetCanMovePlayer(bool val)
+	{
+		yield return new WaitForEndOfFrame ();
+		yield return new WaitForSeconds (.1f);
+		canMovePlayer = val;
+		yield return null;
+	}
+
+	public void CustomSetUIButtonState(UIButton button, bool state)
+	{
+		button.enabled = state;	
+		if(state)
+			button.SetState(UIButtonColor.State.Normal, true);
+		else
+			button.SetState(UIButtonColor.State.Disabled, true);
+	}
+
+	public void SetWinnerLabel(string name)
+	{
+		winnerDisplayLabel.text = name + " won the game, much good";
+	}
+
+
+}
